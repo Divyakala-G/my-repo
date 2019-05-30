3cf6e8da06fc98b197996c292148a9910a4c2dec
MK <MK>
2019-05-31 00:17:16 +1000

    first commit

---
+++ src/main/java/com/anz/my_app_anz/App.java
@@ -0,0 +1,13 @@
+package com.anz.my_app_anz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
